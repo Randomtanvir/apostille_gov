b5e62d457425eb41bce780d859cc6bc6f6ed4e48
Tanvir <tanvir.info07@gmail.com>
2025-05-18 16:20:20 +0600

    env file give path for image

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
       <Image
         width={205}
         height={205}
-        src="/header_logo.png"
+        src={`${process.env.NEXT_PUBLIC_IMAGE_URL}/header_logo.png`}
         alt="Logo"
         className="mt-4 ml-4"
       />
